3243054eb7714aa3282e5426efda32317462c032
Daniel Hindi <Danny@FlywheelNow.com>
2015-06-12 19:09:46 -0700

    use app config appid

--- pluginTester/scripts/overrides.js
+++ pluginTester/scripts/overrides.js
@@ -2,7 +2,7 @@
  * Created by Daniel on 5/23/2015.
  */
 logger = console;
-pluginAPI.init(-1, 0, 1, 0);
+pluginAPI.init(window.appConfig.appId, 0, 1, 0);
 pluginAPI.datastore.dataStoreUrl = 'http://ds.buildfire.com:88';
 
 pluginAPI.datastore.onUpdate(function(updateObj){
