21e495f3caea09d48c8d79974fa44d2aef3898ec
Daniel Hindi <Danny@FlywheelNow.com>
2015-07-13 16:38:58 -0700

    fix datastore port reference

--- pluginTester/scripts/siteConfig.js
+++ pluginTester/scripts/siteConfig.js
@@ -1,7 +1,7 @@
 window.siteConfig = {
     appId : 202
     , endPoints : {
-        datastoreHost: 'http://int2.ds.buildfire.com'
+        datastoreHost: 'http://int2.ds.buildfire.com:88'
     }
     ,keys: {
         datastoreKey:'123-ASD-TEST'
