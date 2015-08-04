c6ebe6af7e4ea15657f7dd80a3495e481d126204
Daniel Hindi <Danny@FlywheelNow.com>
2015-08-04 14:48:12 -0700

    give better launcher example. still not ready for prime time

--- plugins/exampleLauncherPlugin/widget/homeCtrl.js
+++ plugins/exampleLauncherPlugin/widget/homeCtrl.js
@@ -3,9 +3,12 @@ var counter=0;
 $home.controller('homeCtrl', ['$scope'
     , function ($scope) {
 
-        $scope.logoUrl = "http://startupweekend.org/wp-content/blogs.dir/1/files/2013/04/CokeLogo1.png";
-        $scope.images = ["http://www.pd4pic.com/images/black-design-banner-blank-ornate-decoration.png"
-            ,"http://www.pd4pic.com/images/fancy-design-banner-curved-blank-decoration.png"];
+        $scope.logoUrl = "https://s3.amazonaws.com/kaleo.live/mobile/whitelabel/56898399-b378-4ca8-bab7-e75aba7c43a7.png";
+        $scope.images = ["http://rack.3.mshcdn.com/media/ZgkyMDEzLzEyLzAzL2M3L01vYmlsZVBsYXRmLmE5NjIxLmpwZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/4412ed4b/9e6/MobilePlatforms.jpg"];
+
+        for(var i=0;i< $scope.images.length ;i++)
+            $scope.images[i]= buildfire.imageLib.resizeImage($scope.images[i],{width:400});
+
 
         $scope.appContentId = 123;
 
@@ -19,6 +22,8 @@ $home.controller('homeCtrl', ['$scope'
             ,{click:fn,title:"widget2", widgetId:'examplePlugin',instanceId:321, icon:"http://www.canadatechblog.com/wp-content/uploads/2013/08/mzl.iqdiiykr.png", requiresConnection:true}
         ];
 
+        for(var i=0;i< widgets.length ;i++)
+            widgets[i].icon= buildfire.imageLib.resizeImage(widgets[i].icon,{width:64});
         $scope.widgets=widgets;
 
     }
