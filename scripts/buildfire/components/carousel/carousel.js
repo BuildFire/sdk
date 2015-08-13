846763fb6f5ceb57ba1f1ebe0a6cac8965eff155
Ahmed <ahmed.it.hmoud@gmail.com>
2015-08-13 16:40:38 -0700

    Add sortable plug-in to the scripts folder Implement the basic sorting functionality Implement add new slider functionality without saving it in the items

--- scripts/buildfire/components/carousel/carousel.js
+++ scripts/buildfire/components/carousel/carousel.js
@@ -1,8 +1,22 @@
 ﻿'use strict';
+// Add to dummy items for testing purposes
+var helpItem = [{
+    "actionName": "Link to Web Content", "action": "linkToWeb", "openIn": "_blank", "title": "ONE",
+    "iconUrl": "https://imagelibserver.s3.amazonaws.com/1439498230636-034027228760533035/d4210610-4202-11e5-b604-e5d8df00b595.jpg",
+    "url": "http://www.developercode.net"
+}, {
+    "actionName": "Link to Web Content", "action": "linkToWeb", "openIn": "_blank", "title": "ONE",
+    "iconUrl": "https://imagelibserver.s3.amazonaws.com/1439498230636-034027228760533035/d4210610-4202-11e5-b604-e5d8df00b595.jpg",
+    "url": "http://www.developercode.net"
+}];
 
 if (typeof (buildfire) == "undefined") throw ("please add buildfire.js first to use carousel components");
 
-if (typeof ($) == "undefined") throw ("please add JQuery first to use carousel components")
+// jQuery me be need later, please uncomment this line when you want to use jQuery in this component
+// if (typeof ($) == "undefined") throw ("please add JQuery first to use carousel components");
+
+if (typeof (Sortable) == "undefined") throw ("please add Sortable first to use carousel components");
+
 
 if (typeof (buildfire.components) == "undefined")
     buildfire.components = {};
@@ -10,6 +24,7 @@ if (typeof (buildfire.components) == "undefined")
 if (typeof (buildfire.components.carousel) == "undefined")
     buildfire.components.carousel = {};
 
+// This is the class that will be used in the plugin content, design, or settings sections
 buildfire.components.carousel.editor = function (selector, items) {
     this.selector = selector;
 
@@ -23,6 +38,7 @@ buildfire.components.carousel.editor = function (selector, items) {
     this.init(selector);
 }
 
+// Carousel Editor methods
 buildfire.components.carousel.editor.prototype = {
     onItemChange: function (item) {
         throw ("please handle onAddItem");
@@ -39,11 +55,64 @@ buildfire.components.carousel.editor.prototype = {
         console.info(this.selector)
         this._loadTemplate(function (html) {
             self.selector.innerHTML = html;
-            self.itemTemplate = self.selector.querySelector("#itemTemplate").innerHTML;
             self.itemsContainer = self.selector.querySelector(".carousel-items");
-            self.itemsContainer.innerHTML = self.itemTemplate;
+            // add to items to the component for testing purposes
+            self._appendItem(helpItem[0]);
+            self._appendItem(helpItem[1]);
             self._initEvents();
         });
+    },
+    _appendItem: function (item) {
+        
+        var self = this,
+            // Create the required DOM elements
+            wrapper = document.createElement("div"),
+            moveHandle = document.createElement("span"),
+            mediaHolder = document.createElement("div"),
+            image = document.createElement("img"),
+            details = document.createElement("div"),
+            title = document.createElement("span"),
+            editButton = document.createElement("a"),
+            deleteButton = document.createElement("span");
+
+        // Add the required classes to the elements
+        wrapper.className = "d-item";
+        moveHandle.className = "icon icon-menu pull-left";
+        mediaHolder.className = "media-holder pull-left";
+        image.className = "border-radius-four border-grey";
+        details.className = "copy pull-right";
+        title.className = "title ellipsis";
+        editButton.className = "text-primary text";
+        deleteButton.className = "btn-icon btn-delete-icon btn-danger transition-third";
+
+        image.src = item.iconUrl;
+        title.innerText = item.title;
+        editButton.innerText = "Edit";
+
+        // Append elements to the DOM
+        wrapper.appendChild(moveHandle);
+        wrapper.appendChild(mediaHolder);
+        mediaHolder.appendChild(image);
+        details.appendChild(title);
+        details.appendChild(editButton);
+        details.appendChild(deleteButton);
+        wrapper.appendChild(details);
+        self.itemsContainer.appendChild(wrapper);
+
+        // initialize the click events on the current item
+        editButton.addEventListener("click", (function (item) {
+            return function (e) {
+                e.preventDefault();
+                console.warn("Editing item: ", item);
+            };
+        })(item));
+
+        deleteButton.addEventListener("click", (function (item) {
+            return function (e) {
+                e.preventDefault();
+                console.warn("Editing item: ", item);
+            };
+        })(item));
     },
     _getDomSelector: function (selector) {
         if (selector && selector.nodeType && selector.nodeType === 1) {
@@ -79,16 +148,27 @@ buildfire.components.carousel.editor.prototype = {
     },
     _initEvents: function () {
         var self = this;
+
         // initialize add new item button
         self.selector.querySelector(".add-new-carousel").addEventListener("click", function () {
             buildfire.actionItems.showDialog(null, { showIcon: true }, function (err, actionItem) {
                 if (err)
                     console.error("Error adding a new carousel: ", err);
                 else {
-                    console.info(actionItem);
+                    self._appendItem(actionItem);
                 }
             });
+        });
 
+        // initialize the sort on the container of the items
+        self.sortableList = Sortable.create(self.itemsContainer, {
+            animation: 150,
+            onAdd: function (evt) { console.log('onAdd.foo:', [evt.item, evt.from]); },
+            onUpdate: function (evt) { console.log('onUpdate.foo:', [evt.item, evt.from]); },
+            onRemove: function (evt) { console.log('onRemove.foo:', [evt.item, evt.from]); },
+            onStart: function (evt) { console.log('onStart.foo:', [evt.item, evt.from]); },
+            onSort: function (evt) { console.log('onStart.foo:', [evt.item, evt.from]); },
+            onEnd: function (evt) { console.log('onEnd.foo:', [evt.item, evt.from]); }
         });
     }
 };
