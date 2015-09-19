a8ebabfffc1b12773e42695544c09029d8a6da1d
Ahmed <ahmed.it.hmoud@gmail.com>
2015-09-19 12:40:32 -0700

    Add load all plugins to plugin instances implement buildfire.notifications.confirm

---
+++ scripts/buildfire/components/notifications/notifications.js
@@ -0,0 +1,86 @@
+﻿(function () {
+    'use strict';
+    var confirmComponent = null;
+    if (typeof (buildfire) == "undefined") throw ("please add buildfire.js first to use sortableList components");
+
+    if (typeof (buildfire.notifications) == "undefined")
+        buildfire.notifications = {};
+
+    // This is the class that will be used in the plugin content, design, or settings sections
+    buildfire.notifications.confirm = function (options, callback) {
+
+        if (!options || typeof (options) !== "object") {
+            throw "You must provide a options of type provide buildfire.notifications.confirm or {} if you have no options";
+        }
+        if (!callback || typeof (callback) !== "function") {
+            throw "You must provide a callback of type function to buildfire.notifications.confirm";
+        }
+
+        if (!confirmComponent.confirmContainer) {
+            confirmComponent._appendContainer();
+        }
+        confirmComponent._renderTemplate(options);
+        confirmComponent.initEvents(callback);
+    };
+
+    confirmComponent = buildfire.notifications.confirm;
+
+    confirmComponent.confirmContainer = null;
+
+    confirmComponent._appendContainer = function () {
+        var container = document.createElement("div"),
+            containerId = "confirm" + Math.floor((Math.random() * 10) + 1);
+        container.setAttribute("id", containerId);
+        document.body.appendChild(container);
+        confirmComponent.confirmContainer = document.querySelector("#" + containerId);
+    };
+
+    confirmComponent._renderTemplate = function (options) {
+        var html = '<div class="modal-backdrop fade in" style="z-index:1040;"></div>\
+            <div class="modal fade in" style="z-index:1050; display:block;">\
+                <div class="modal-dialog">\
+                    <div class="modal-content">\
+                        <div class="modal-header">\
+                            <h5 class="item-link-header margin-zero">#title</h5>\
+                        </div>\
+                        <div class="modal-body">\
+                            #message\
+                        </div>\
+                        <div class="bottom border-top-grey clearfix">\
+                            <div class="col-md-6 pull-left text-center">\
+                                <a class="text-primary default-background-hover transition-third cancel-confirmation">#cancel</a>\
+                            </div>\
+                            <div class="col-md-6 pull-right text-center border-left-grey">\
+                                <a class="text-danger default-background-hover transition-third approve-confirmation">#ok</a>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>'
+            .replace("#title", options.title ? options.title : "Confirm")
+            .replace("#message", options.message ? options.message : "Confirm")
+            .replace("#ok", options.buttonLabels && options.buttonLabels[0] ? options.buttonLabels[0] : "OK")
+            .replace("#cancel", options.buttonLabels && options.buttonLabels[1] ? options.buttonLabels[1] : "Cancel");
+
+        confirmComponent.confirmContainer.innerHTML = html;
+    };
+
+    confirmComponent._removeTemplate = function () {
+        confirmComponent.confirmContainer.innerHTML = "";
+    };
+
+    confirmComponent.initEvents = function (callback) {
+
+        confirmComponent.confirmContainer.querySelector(".approve-confirmation").addEventListener("click", function (e) {
+            e.preventDefault();
+            callback(e);
+            confirmComponent._removeTemplate();
+        });
+
+        confirmComponent.confirmContainer.querySelector(".cancel-confirmation").addEventListener("click", function (e) {
+            e.preventDefault();
+            confirmComponent._removeTemplate();
+        });
+
+    };
+})();
