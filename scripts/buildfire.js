b6d1ffebd916d24877880b31c27b12a1bf727da0
Daniel Hindi <daniel@madaincorp.com>
2015-10-22 09:21:48 -0700

    attach fastclick to widgets

--- scripts/buildfire.js
+++ scripts/buildfire.js
@@ -239,7 +239,6 @@ var buildfire = {
                 }
         }
     }
-
     , context: null
     , init: function () {
         // Listen to message from child window
@@ -262,7 +261,7 @@ var buildfire = {
             }
         });
 
-        buildfire._insertHTMLAttributes();
+        buildfire.appearance.insertHTMLAttributes();
         buildfire.appearance.attachCSSFiles();
     }
     , _whitelistedCommands: ["datastore.triggerOnUpdate"
@@ -364,7 +363,80 @@ var buildfire = {
         }
     }
     , appearance: {
-        getCSSFiles: function (callback) {
+         insertHTMLAttributes: function () {
+            var html = document.getElementsByTagName('html')[0];
+            html.setAttribute('buildfire', 'enabled');
+
+            var nVer = navigator.appVersion;
+            var nAgt = navigator.userAgent;
+            var browserName = navigator.appName;
+            var fullVersion = '' + parseFloat(navigator.appVersion);
+            var majorVersion = parseInt(navigator.appVersion, 10);
+            var nameOffset, verOffset, ix;
+            var os = "Unknown OS";
+// In Opera, the true version is after "Opera" or after "Version"
+            if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+                browserName = "Opera";
+                fullVersion = nAgt.substring(verOffset + 6);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+// In MSIE, the true version is after "MSIE" in userAgent
+            else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+                browserName = "Microsoft Internet Explorer";
+                fullVersion = nAgt.substring(verOffset + 5);
+            }
+// In Chrome, the true version is after "Chrome"
+            else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+                browserName = "Chrome";
+                fullVersion = nAgt.substring(verOffset + 7);
+            }
+// In Safari, the true version is after "Safari" or after "Version"
+            else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+                browserName = "Safari";
+                fullVersion = nAgt.substring(verOffset + 7);
+                if ((verOffset = nAgt.indexOf("Version")) != -1)
+                    fullVersion = nAgt.substring(verOffset + 8);
+            }
+// In Firefox, the true version is after "Firefox"
+            else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+                browserName = "Firefox";
+                fullVersion = nAgt.substring(verOffset + 8);
+            }
+// In most other browsers, "name/version" is at the end of userAgent
+            else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+                (verOffset = nAgt.lastIndexOf('/'))) {
+                browserName = nAgt.substring(nameOffset, verOffset);
+                fullVersion = nAgt.substring(verOffset + 1);
+                if (browserName.toLowerCase() == browserName.toUpperCase()) {
+                    browserName = navigator.appName;
+                }
+            }
+// trim the fullVersion string at semicolon/space if present
+            if ((ix = fullVersion.indexOf(";")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+            if ((ix = fullVersion.indexOf(" ")) != -1)
+                fullVersion = fullVersion.substring(0, ix);
+
+            majorVersion = parseInt('' + fullVersion, 10);
+            if (isNaN(majorVersion)) {
+                fullVersion = '' + parseFloat(navigator.appVersion);
+                majorVersion = parseInt(navigator.appVersion, 10);
+            }
+
+
+            if (navigator.appVersion.indexOf("Win") != -1) os = "Windows";
+            if (navigator.appVersion.indexOf("Mac") != -1) os = "MacOS";
+            if (navigator.appVersion.indexOf("X11") != -1) os = "UNIX";
+            if (navigator.appVersion.indexOf("Linux") != -1)os = "Linux";
+
+            html.setAttribute('os', os);
+            html.setAttribute('browser', browserName);
+            html.setAttribute('majorVersion', majorVersion);
+            html.setAttribute('fullVersion', fullVersion);
+
+        }
+        , getCSSFiles: function (callback) {
             var p = new Packet(null, 'appearance.getCSSFiles');
             buildfire._sendPacket(p, callback);
         }
@@ -390,7 +462,43 @@ var buildfire = {
             for (var i = 0; i < files.length; i++)
                 document.write('<link rel="stylesheet" href="' + base + files[i] + '"/>');
 
+        }
+        , attachFastClick: function(){
 
+            var path;
+            var scripts = document.getElementsByTagName("script");
+            for (var i = 0; i < scripts.length; i++) {
+                if (scripts[i].src.indexOf('buildfire.js') > 0)
+                    path = scripts[i].src.replace('buildfire.js', 'fastclick.js');
+                else if (scripts[i].src.indexOf('fastclick.js') > 0){
+                    console.warn('fastclick already attached');
+                    return;
+                }
+            }
+            if(!path){
+                console.error('buildfire.js not found? This should never happen. I cant trust anything anymore.. mommy!');
+                return;
+            }
+            else {
+                var script = document.createElement('script');
+                script.src = path;
+                script.type="text/javascript";
+                script.onload=function(){
+                    console.info('fastclick.js loaded');
+                    if(typeof(FastClick) == "undefined")
+                        console.error('fastclick undefined');
+                    else
+                        FastClick.attach(document.body);
+                };
+                document.body.appendChild(script);
+            }
+        }
+        , applyFastClick: function(element){
+            if(!element)element=document.body;
+            if(typeof(FastClick) == "undefined")
+                console.error('fastclick undefined');
+            else
+                FastClick.attach(element);
         }
         , attachAppThemeCSSFiles: function (appId, liveMode, appHost) {
             var linkElement = document.createElement("link");
@@ -818,79 +926,6 @@ var buildfire = {
             buildfire._sendPacket(new Packet(null, 'spinner.hide'));
         }
     }
-    , _insertHTMLAttributes: function () {
-        var html = document.getElementsByTagName('html')[0];
-        html.setAttribute('buildfire', 'enabled');
-
-        var nVer = navigator.appVersion;
-        var nAgt = navigator.userAgent;
-        var browserName = navigator.appName;
-        var fullVersion = '' + parseFloat(navigator.appVersion);
-        var majorVersion = parseInt(navigator.appVersion, 10);
-        var nameOffset, verOffset, ix;
-        var os = "Unknown OS";
-// In Opera, the true version is after "Opera" or after "Version"
-        if ((verOffset = nAgt.indexOf("Opera")) != -1) {
-            browserName = "Opera";
-            fullVersion = nAgt.substring(verOffset + 6);
-            if ((verOffset = nAgt.indexOf("Version")) != -1)
-                fullVersion = nAgt.substring(verOffset + 8);
-        }
-// In MSIE, the true version is after "MSIE" in userAgent
-        else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
-            browserName = "Microsoft Internet Explorer";
-            fullVersion = nAgt.substring(verOffset + 5);
-        }
-// In Chrome, the true version is after "Chrome"
-        else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
-            browserName = "Chrome";
-            fullVersion = nAgt.substring(verOffset + 7);
-        }
-// In Safari, the true version is after "Safari" or after "Version"
-        else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
-            browserName = "Safari";
-            fullVersion = nAgt.substring(verOffset + 7);
-            if ((verOffset = nAgt.indexOf("Version")) != -1)
-                fullVersion = nAgt.substring(verOffset + 8);
-        }
-// In Firefox, the true version is after "Firefox"
-        else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
-            browserName = "Firefox";
-            fullVersion = nAgt.substring(verOffset + 8);
-        }
-// In most other browsers, "name/version" is at the end of userAgent
-        else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
-            (verOffset = nAgt.lastIndexOf('/'))) {
-            browserName = nAgt.substring(nameOffset, verOffset);
-            fullVersion = nAgt.substring(verOffset + 1);
-            if (browserName.toLowerCase() == browserName.toUpperCase()) {
-                browserName = navigator.appName;
-            }
-        }
-// trim the fullVersion string at semicolon/space if present
-        if ((ix = fullVersion.indexOf(";")) != -1)
-            fullVersion = fullVersion.substring(0, ix);
-        if ((ix = fullVersion.indexOf(" ")) != -1)
-            fullVersion = fullVersion.substring(0, ix);
-
-        majorVersion = parseInt('' + fullVersion, 10);
-        if (isNaN(majorVersion)) {
-            fullVersion = '' + parseFloat(navigator.appVersion);
-            majorVersion = parseInt(navigator.appVersion, 10);
-        }
-
-
-        if (navigator.appVersion.indexOf("Win") != -1) os = "Windows";
-        if (navigator.appVersion.indexOf("Mac") != -1) os = "MacOS";
-        if (navigator.appVersion.indexOf("X11") != -1) os = "UNIX";
-        if (navigator.appVersion.indexOf("Linux") != -1)os = "Linux";
-
-        html.setAttribute('os', os);
-        html.setAttribute('browser', browserName);
-        html.setAttribute('majorVersion', majorVersion);
-        html.setAttribute('fullVersion', fullVersion);
-
-    }
     , auth: {
         login: function (options, callback) {
             var p = new Packet(null, 'auth.login', options);
@@ -931,6 +966,9 @@ buildfire.init();
 
 document.addEventListener("DOMContentLoaded", function (event) {
     buildfire.appearance.autosizeContainer();
+    console.info('DOMContentLoaded');
+    if(window.location.href.indexOf('/widget/'))
+        buildfire.appearance.attachFastClick();
 });
 document.addEventListener("resize", function (event) {
     buildfire.appearance.autosizeContainer();
