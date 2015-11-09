6e01af722aa0dfc034de0b70ee89047b3d5c26ce
Daniel Hindi <daniel@madaincorp.com>
2015-11-08 23:03:05 -0800

    settings save

--- pluginTester/pages/controllers/settingsCtrl.js
+++ pluginTester/pages/controllers/settingsCtrl.js
@@ -6,4 +6,13 @@ $app.controller('settingsCtrl', ['$scope', function ($scope) {
 
     $scope.appContext=JSON.parse(JSON.stringify(window.appContext)) ;// make a copy
 
+    var tmr;
+    $scope.$watch('appContext',function(newContext){
+        if(tmr)clearTimeout(tmr);
+        tmr = setTimeout(function(){
+            window.appContext = newContext;
+            window.localStorage.setItem('appContext',JSON.stringify(window.appContext));
+        }
+        ,500);
+    },true);
 }]);
