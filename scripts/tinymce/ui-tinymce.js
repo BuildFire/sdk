5363c4f6eaee702cb9627a440c7e9c6c4ca741ca
Vishnu Singh <vish250491@gmail.com>
2016-04-22 12:41:41 +0530

    tinymce updated for providing WSYIWYG forcolor and backcolor support

--- scripts/tinymce/ui-tinymce.js
+++ scripts/tinymce/ui-tinymce.js
@@ -10,64 +10,64 @@ angular.module('ui.tinymce', [])
         if (uiTinymceConfig.baseUrl) {
             tinymce.baseURL = uiTinymceConfig.baseUrl;
         }
-        
+
         return {
             require: ['ngModel', '^?form'],
             link: function (scope, element, attrs, ctrls) {
                 if (!$window.tinymce) {
                     return;
                 }
-                
+
                 var ngModel = ctrls[0],
                     form = ctrls[1] || null;
-                
+
                 var expression, options, tinyInstance,
                     updateView = function (editor) {
                         var content = editor.getContent({ format: options.format }).trim();
                         content = $sce.trustAsHtml(content);
-                        
+
                         ngModel.$setViewValue(content);
                         if (!$rootScope.$$phase) {
                             scope.$apply();
                         }
                     };
-                
+
                 function toggleDisable(disabled) {
                     if (disabled) {
                         ensureInstance();
-                        
+
                         if (tinyInstance) {
                             tinyInstance.getBody().setAttribute('contenteditable', false);
                         }
                     } else {
                         ensureInstance();
-                        
+
                         if (tinyInstance) {
                             tinyInstance.getBody().setAttribute('contenteditable', true);
                         }
                     }
                 }
-                
+
                 // generate an ID
                 attrs.$set('id', ID_ATTR + '-' + generatedIds++);
-                
+
                 expression = {};
-                
+
                 angular.extend(expression, scope.$eval(attrs.uiTinymce));
-                
+
                 options = {
                     selector: 'textarea',  // change this value according to your HTML
-                    toolbar: ['  styleselect  | insertfile undo redo  | bold italic | alignleft aligncenter alignright alignjustify  | bullist  numlist | outdent indent ',
-                        ' link image | media  | code'
+                    toolbar: ['  styleselect  | insertfile  | forecolor | backcolor | bold italic | alignleft aligncenter alignright alignjustify  | bullist  numlist | outdent indent ',
+                        ' link image | media  | code  '
                     ],
-                    
+
                     menu: {
                         edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall' },
                         insert: { title: 'Insert', items: 'link image | media  | code' },
                         view   : { title : 'View'  , items : 'preview' },
                         format : { title : 'Format', items : 'bold italic underline strikethrough superscript subscript | formats | removeformat' },
                     },
-                    
+
                     // Update model when calling setContent
                     // (such as from the source editor popup)
                     setup: function (ed) {
@@ -78,33 +78,33 @@ angular.module('ui.tinymce', [])
                                 form.$setPristine();
                             }
                         });
-                        
+
                         // Update model on button click
                         ed.on('ExecCommand', function () {
                             ed.save();
                             updateView(ed);
                         });
-                        
+
                         // Update model on change
                         ed.on('change', function (e) {
                             ed.save();
                             updateView(ed);
                         });
-                        
+
                         ed.on('blur', function () {
                             element[0].blur();
                         });
-                        
+
                         // Update model when an object has been resized (table, image)
                         ed.on('ObjectResized', function () {
                             ed.save();
                             updateView(ed);
                         });
-                        
+
                         ed.on('remove', function () {
                             element.remove();
                         });
-                        
+
                         if (expression.setup) {
                             expression.setup(ed, {
                                 updateView: updateView
@@ -121,25 +121,26 @@ angular.module('ui.tinymce', [])
                 // element to be present in DOM before instantiating editor when
                 // re-rendering directive
                 $timeout(function () {
-                    options.plugins = "preview , image,code,media,link";
+                    options.plugins = "preview , image,code,media,link , textcolor colorpicker";
+                    // options.toolbar= "preview ,image,code,media,link , forecolor "
                     tinymce.init(options);
                     toggleDisable(scope.$eval(attrs.ngDisabled));
                 });
-                
+
                 ngModel.$formatters.unshift(function (modelValue) {
                     return modelValue ? $sce.trustAsHtml(modelValue) : '';
                 });
-                
+
                 ngModel.$parsers.unshift(function (viewValue) {
                     return viewValue ? $sce.getTrustedHtml(viewValue) : '';
                 });
-                
+
                 ngModel.$render = function () {
                     ensureInstance();
-                    
+
                     var viewValue = ngModel.$viewValue ?
                         $sce.getTrustedHtml(ngModel.$viewValue) : '';
-                    
+
                     // instance.getDoc() check is a guard against null value
                     // when destruction & recreation of instances happen
                     if (tinyInstance &&
@@ -151,9 +152,9 @@ angular.module('ui.tinymce', [])
                         tinyInstance.fire('change');
                     }
                 };
-                
+
                 attrs.$observe('disabled', toggleDisable);
-                
+
                 // This block is because of TinyMCE not playing well with removal and
                 // recreation of instances, requiring instances to have different
                 // selectors in order to render new instances properly
@@ -169,16 +170,16 @@ angular.module('ui.tinymce', [])
                         parentElement.append($compile(clonedElement)(scope));
                     }
                 });
-                
+
                 scope.$on('$destroy', function () {
                     ensureInstance();
-                    
+
                     if (tinyInstance) {
                         tinyInstance.remove();
                         tinyInstance = null;
                     }
                 });
-                
+
                 function ensureInstance() {
                     if (!tinyInstance) {
                         tinyInstance = tinymce.get(attrs.id);
