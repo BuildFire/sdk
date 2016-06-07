2ed51c2f74e0754a17d36584c4b672ebccdb2596
attila buturla <abuturla@buildfire.com>
2016-06-07 08:29:57 -0700

    WIP update

--- scripts/buildfire/services/notifications/localNotifications.js
+++ scripts/buildfire/services/notifications/localNotifications.js
@@ -1,8 +1,10 @@
 /**
  * Created by danielhindi on 2/19/16.
  */
+
 if (typeof (buildfire) == "undefined") throw ("please add buildfire.js first to use BuildFire services");
 
+console.log('*** localNotifications.js 2 ***');
 
 buildfire.notifications.localNotification = {
     send: function (options, callback) {
@@ -14,23 +16,14 @@ buildfire.notifications.localNotification = {
             text: options.text
         };
 
-        var packet = new Packet(packetId, command, data);
+        var packet = new Packet(packetId, command, options);
         buildfire._sendPacket(packet, callback);
     }
     , schedule: function (options, callback) {
         var packetId = null;
         var command = 'localNotifications.schedule';
-        //https://github.com/katzer/cordova-plugin-local-notifications/wiki/04.-Scheduling
-        var data = {
-            id: options.id,
-            title: options.title,
-            text: options.text,
-            firstAt: options.firstAt
-        };
 
-        //firstAt: options.firstAt
-
-        var packet = new Packet(packetId, command, data);
+        var packet = new Packet(packetId, command, options);
         buildfire._sendPacket(packet, callback);
     }
     , hasPermission: function (options, callback) {
