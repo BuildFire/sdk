137b1259501fafe3d209e5dbab0f7322f14d9dc4
Ayman Habeb <ahabeb@madaincorp.com>
2017-04-18 22:45:01 +0300

    adding camera API to buildfire services

---
+++ scripts/buildfire/services/camera/core.js
@@ -0,0 +1,19 @@
+/**
+ * Created by aymanhabeb on 16/4/17.
+ */
+
+
+if (typeof (buildfire) == "undefined") throw ("please add buildfire.js first to use BuildFire services");
+
+if (typeof (buildfire.services) == "undefined") buildfire.services = {};
+
+if (typeof (buildfire.services.camera) == "undefined") buildfire.services.camera = {};
+
+
+buildfire.services.camera.core = {
+    getPicture: function (options, callback) {
+        if (!options)
+            options = {};
+        buildfire._sendPacket(new Packet(null, "camera.getPicture", options), callback);
+    }
+};
