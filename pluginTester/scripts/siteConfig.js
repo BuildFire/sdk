4763358897c34b9d176cbfcd503c84907d607dcc
attila buturla <abuturla@buildfire.com>
2017-07-31 16:58:09 -0700

    Updated app endpoint to use HTTPS

--- pluginTester/scripts/siteConfig.js
+++ pluginTester/scripts/siteConfig.js
@@ -2,7 +2,7 @@ window.siteConfig = {
 	appId: 202
 	, endPoints: {
 		datastoreHost: 'http://datastore.buildfire.com',
-		appHost: 'http://app.buildfire.com',
+		appHost: 'https://app.buildfire.com',
 		auth: "auth.buildfire.com",
 		authHost: "http://auth.buildfire.com",
 		pluginHost : "http://s3-us-west-2.amazonaws.com/pluginserver.prod/plugins",
