45a70db8e1d8de63ead8f2d965afbfa5efa29370
Rami Abdel Hadi <rabdelhadi@madaincorp.com>
2018-08-14 01:21:53 +0400

    add publicFiles service

--- scripts/buildfire.js
+++ scripts/buildfire.js
@@ -143,6 +143,7 @@ var buildfire = {
         , "navigation.triggerOnAppLauncherActive"
         , "navigation.triggerOnAppLauncherInactive"
         , "colorLib._triggerOnChange"
+        , "publicFiles._triggerOnProgress"
     ]
     , _postMessageHandler: function (e) {
         if (e.source === window) {
---
+++ scripts/buildfire/services/publicFiles/publicFiles.js
@@ -0,0 +1,16 @@
+if (typeof (buildfire) == "undefined") throw ("please add buildfire.js first to use BuildFire services");
+
+if (typeof (buildfire.services) == "undefined") buildfire.services = {};
+
+if (typeof (buildfire.services.publicFiles) == "undefined")
+    buildfire.services.publicFiles = {};
+
+buildfire.services.publicFiles.upload = function(options,onProgress , callback) {
+    buildfire.eventManager.clear('publicFilesOnProgress');
+    var p = new Packet(null, 'publicFiles.upload', {options: options});
+    buildfire._sendPacket(p, callback);
+};
+
+buildfire.services.publicFiles._triggerOnProgress = function (data) {
+    buildfire.eventManager.trigger('publicFilesOnProgress', data);
+}
