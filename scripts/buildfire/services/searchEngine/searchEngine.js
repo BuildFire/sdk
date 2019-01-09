91420b96791462e44eb0dfa4ba60c7dac57823fc
Ahmad Hamed <ahmad.fhamed@gmail.com>
2019-01-10 00:02:36 +0200

    update comments in search engine

--- scripts/buildfire/services/searchEngine/searchEngine.js
+++ scripts/buildfire/services/searchEngine/searchEngine.js
@@ -11,13 +11,13 @@ if (typeof (buildfire.services.searchEngine) == "undefined") buildfire.services.
 /**
  * Insert data in buildfire search engine.
  * @param {Object} options - insert options.
- * @param {boolean=} options.linkedUser - This will make the data linked to the current logged user, which means it will be private.
+ * @param {boolean} [options.linkedUser] - This will make the data linked to the current logged user, which means it will be private.
  * @param {string} options.tag - A unique key for your data, this is important for categorizing your data.
  * @param {string} options.title - Title for your data, this will be searchable by our search engine.
- * @param {string=} options.description - Description for your data, this will be searchable by our search engine.
- * @param {string=} options.keywords - Any keywords related to your data, this will be searchable by our search engine.
- * @param {string=} options.image_url.
- * @param {Object=} options.data - You can add whatever you want here, this won't be searchable by our search engine.
+ * @param {string} [options.description] - Description for your data, this will be searchable by our search engine.
+ * @param {string} [options.keywords] - Any keywords related to your data, this will be searchable by our search engine.
+ * @param {string} [options.image_url].
+ * @param {Object} [options.data] - You can add whatever you want here, this won't be searchable by our search engine.
  */
 
 /**
@@ -38,13 +38,13 @@ buildfire.services.searchEngine.insert = function(options, cb){
  * Update data in buildfire search engine.
  * @param {Object} options - update options.
  * @param {string} options.id - An id for your document to update it.
- * @param {boolean=} options.linkedUser - This will make the data linked to the current logged user, which means it will be private.
+ * @param {boolean=} [options.linkedUser] - This will make the data linked to the current logged user, which means it will be private.
  * @param {string} options.tag - A unique key for your data, this is important for categorizing your data.
  * @param {string} options.title - Title for your data, this will be searchable by our search engine.
- * @param {string=} options.description - Description for your data, this will be searchable by our search engine.
- * @param {string=} options.keywords - Any keywords related to your data, this will be searchable by our search engine.
- * @param {string=} options.image_url.
- * @param {Object=} options.data - You can add whatever you want here, this won't be searchable by our search engine.
+ * @param {string} [options.description] - Description for your data, this will be searchable by our search engine.
+ * @param {string} [options.keywords] - Any keywords related to your data, this will be searchable by our search engine.
+ * @param {string} [options.image_url].
+ * @param {Object} [options.data] - You can add whatever you want here, this won't be searchable by our search engine.
  */
 
 /**
@@ -62,6 +62,27 @@ buildfire.services.searchEngine.update = function(options, cb){
     buildfire._sendPacket(packet, cb);
 };
 
+/**
+ * Search data in buildfire search engine.
+ * @param {Object} options - search options.
+ * @param {string} options.search_text - Your search text.
+ * @param {boolean} [options.linkedUser] - If true this will return all public data and the data added by the current logged user.
+ * @param {string} [options.page_size = 10]  - Number of data returned per page size.
+ * @param {string} [options.page_index = 0] - Number of returned page.
+ * @param {string} [options.pre_highlight_tag] - Use in conjunction with post_tags to define the HTML tags to use for the highlighted text.
+ * @param {string} [options.post_highlight_tag] - Use in conjunction with pre_tags to define the HTML tags to use for the highlighted text.
+ */
+
+/**
+ * @callback cb
+ * @param {Object} error
+ *
+ * @param {Object} response
+ * @param {Object} response.hits
+ * @param {Object} response.hits.total
+ * @param {Object} response.hits.max_score
+ * @param {Object[]} response.hits.hits - Array of matched documents/
+ */
 buildfire.services.searchEngine.search = function(options, cb){
     var packetId = null;
     var command = 'searchEngine.search';
@@ -73,7 +94,7 @@ buildfire.services.searchEngine.search = function(options, cb){
 buildfire.services.searchEngine.feeds = {
     /**
      * Attach feed in buildfire search engine.
-     * @param {Object} options - insert options.
+     * @param {Object} options - attach options.
      * @param {string} options.tag - A unique key for your data, this is important for categorizing your data.
      * @param {string} options.title - Title for your data.
      * @param {string} options.feed_type - feed type, available types : [rss].
@@ -82,10 +103,10 @@ buildfire.services.searchEngine.feeds = {
      * @param {Object} options.feed_item_config.
      * @param {string} options.feed_item_config.unique_key - Your feed unique id.
      * @param {string} options.feed_item_config.title_key
-     * @param {string=} options.feed_item_config.description_key
-     * @param {string=} options.feed_item_config.url_key
-     * @param {string=} options.feed_item_config.publish_date_key
-     * @param {string=} options.feed_item_config.image_url_key
+     * @param {string} options.feed_item_config.description_key
+     * @param {string} [options.feed_item_config.url_key]
+     * @param {string} [options.feed_item_config.publish_date_key]
+     * @param {string} [options.feed_item_config.image_url_key]
      */
 
     /**
@@ -106,7 +127,7 @@ buildfire.services.searchEngine.feeds = {
      * @param {Object} options - delete options.
      * @param {string} options.tag - A unique key for your data, this is important for categorizing your data.
      * @param {string} options.feed_id - Feed Id returned from feeds.get method.
-     * @param {boolean=} options.remove_feed_data - If true, this will remove all feed data inside the app that's related to this feed.
+     * @param {boolean} [options.remove_feed_data] - If true, this will remove all feed data inside the app that's related to this feed.
      */
 
     /**
