2bedde8c5da7c3d0f2cdb12f28bea08910356267
Ayman Habeb <ahabeb@buildfire.com>
2019-02-25 10:54:20 -0800

    adding new methods for firebase to get root path and to get collection name

--- scripts/buildfire/services/firebase/firebase.js
+++ scripts/buildfire/services/firebase/firebase.js
@@ -16,5 +16,72 @@ buildfire.services.firebase = {
 
         var packet = new Packet(packetId, command, options);
         buildfire._sendPacket(packet, callback);
+    },
+    getRootPath: function (options, callback) {
+        if (typeof (options) === "function") {
+            callback = options;
+            options = undefined;
+        }
+        if (!options)
+            options = {};
+
+        function getRoot() {
+            //buildfireApps/{appId}/{pluginId}/{instanceId}/
+            var rootPath = 'buildfireApps/' + buildfire.services.firebase._context.appId + '/';
+            switch (options.scope) {
+                case "app":
+                    break;
+                case "plugin":
+                    rootPath = rootPath + buildfire.services.firebase._context.pluginId + '/';
+                    break;
+                case "instance":
+                default:
+                    rootPath = rootPath + buildfire.services.firebase._context.pluginId + '/' + buildfire.services.firebase._context.instanceId + '/';
+                    break;
+            }
+            callback(null, rootPath);
+        }
+
+        if (buildfire.services.firebase._context) {
+            getRoot();
+        } else {
+            buildfire.getContext(function (err, result) {
+                if (err) {
+                    callback(err);
+                } else {
+                    if (result && result.appId) {
+                        buildfire.services.firebase._context = result;
+                        getRoot();
+                    } else {
+                        callback({message: "Invalid appId"});
+                    }
+                }
+            });
+        }
+    },
+    getCollectionPath: function (options, callback) {
+        if (typeof (options) === "function") {
+            callback = options;
+            options = undefined;
+        }
+        if (!options)
+            options = {};
+        buildfire.services.firebase.getRootPath(options, function (err, rootPath) {
+            if (err) {
+                if (callback)
+                    callback(err);
+            } else {
+                if (rootPath) {
+                    callback(null, rootPath + (options.collectionName ? options.collectionName : ''));
+                } else {
+                    callback({message: "Invalid appId"});
+                }
+            }
+        });
     }
 };
+
+//call the getRoot to cache the context and be ready on first load
+buildfire.services.firebase.getRootPath(function (err, result) {
+//do nothing
+});
