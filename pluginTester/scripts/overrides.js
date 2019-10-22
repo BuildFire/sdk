8ada3467c707b7e0ceeea9cd25d215444d517d18
chris <chrisb000@gmail.com>
2019-10-22 13:45:08 -0700

    openProfile feedback on pluginTester

--- pluginTester/scripts/overrides.js
+++ pluginTester/scripts/overrides.js
@@ -84,6 +84,11 @@ postMaster.servicePluginAPIs.service.tag = 'service';
 	    console.warn("supress navigation in shell");
 	    alert("supress navigation in shell");
 	};
+
+	postMaster.controlPluginAPI.auth.openProfile = postMaster.widgetPluginAPI.auth.openProfile = function () {
+		console.warn("supress openProfile in shell");
+		alert("supress openProfile in shell");
+};
 	/**************************************************/
 
 	/*
