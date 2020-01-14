a3d1ad8d989243923736fba2712faa78e8d2280c
Christopher Berger <chrisb000@gmail.com>
2020-01-14 11:10:18 -0800

    Enforce sizes on imageLib

--- scripts/buildfire.js
+++ scripts/buildfire.js
@@ -1521,6 +1521,51 @@ var buildfire = {
     }
     /// ref: https://github.com/BuildFire/sdk/wiki/How-to-use-ImageLib
     , imageLib: {
+        get ENUMS() {
+            return {
+                SIZES: {
+                    xxs: 32,
+                    xs: 64,
+                    s: 128,
+                    m: 200,
+                    l: 304,
+                    xl: 416,
+                    xxl: 1080,
+                    get full() {
+                        return this.findNearest(1);
+                    },
+                    get half() {
+                        return this.findNearest(2);
+                    },
+                    get third() {
+                        return this.findNearest(3);
+                    },
+                    get quarter() {
+                        return this.findNearest(4);
+                    },
+                    findNearest(ratio) {
+                        var match = null;
+                        for (var i = 0; i < this.VALID_SIZES.length; i++) {
+                            const size = this.VALID_SIZES[i];
+
+                            if ((window.innerWidth / ratio) < this[size]) {
+                                match = size;
+                                break;
+                            }
+                        }
+                        return this[match];
+                    },
+                    VALID_SIZES: ['xs', 's', 'm', 'l', 'xl', 'xxl', 'full', 'half', 'third', 'quarter']
+                },
+                ASPECT_RATIOS: {
+                    '1:1': 1,
+                    '4:3': 0.75,
+                    '16:9': 0.5625,
+                    '9:16': 1.77777778,
+                    VALID_RATIOS: ['1:1', '4:3', '16:9', '9:16']
+                }
+            }
+        },
         /// ref: https://github.com/BuildFire/sdk/wiki/How-to-use-ImageLib#buildfireimagelibshowdialogoptions-callback
         showDialog: function (options, callback) {
             var p = new Packet(null, 'imageLib.showDialog', options);
@@ -1587,6 +1632,22 @@ var buildfire = {
                 var compression = buildfire.imageLib.getCompression(options.compression);
                 var result = '';
 
+                if (options.size && options.aspect) {
+                    if (this.ENUMS.SIZES.VALID_SIZES.indexOf(options.size) < 0) {
+                        var sizes = this.ENUMS.SIZES.VALID_SIZES.join(', ');
+                        console.warn('Inavlid size. Availible options are ' + sizes  + '. Returning original url');
+                        return url;
+                    }
+                    if (this.ENUMS.ASPECT_RATIOS.VALID_RATIOS.indexOf(options.aspect) < 0) {
+                        var ratios = this.ENUMS.ASPECT_RATIOS.VALID_RATIOS.join(', ');
+                        console.warn('Inavlid aspect ratio. Availible options are ' + ratios  + '. Returning original url');
+                        return url;
+                    }
+
+                    options.width = this.ENUMS.SIZES[options.size];
+                    options.height = options.width * this.ENUMS.ASPECT_RATIOS[options.aspect];
+                }
+
                 if (options.width && !options.height) {
                     var size = Math.floor(options.width * ratio);
                     result = root + "width/" + size + "/" + compression + url;
@@ -1626,6 +1687,21 @@ var buildfire = {
             }
             if (typeof (options) != "object") {
                 throw ("options not an object");
+            }
+            if (options.size && options.aspect) {
+                if (this.ENUMS.SIZES.VALID_SIZES.indexOf(options.size) < 0) {
+                    var sizes = this.ENUMS.SIZES.VALID_SIZES.join(', ');
+                    console.warn('Inavlid size. Availible options are ' + sizes  + '. Returning original url');
+                    return url;
+                }
+                if (this.ENUMS.ASPECT_RATIOS.VALID_RATIOS.indexOf(options.aspect) < 0) {
+                    var ratios = this.ENUMS.ASPECT_RATIOS.VALID_RATIOS.join(', ');
+                    console.warn('Inavlid aspect ratio. Availible options are ' + ratios  + '. Returning original url');
+                    return url;
+                }
+
+                options.width = this.ENUMS.SIZES[options.size];
+                options.height = options.width * this.ENUMS.ASPECT_RATIOS[options.aspect];
             }
             if (!options.width && !options.height) {
                 options = { width: 'full', height: 'full' };
