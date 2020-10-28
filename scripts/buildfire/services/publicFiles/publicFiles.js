e6c032eeae4fdc20521e697f7f7e899a9edc8a1b
Faruk Alibasic <faruk.alibasic@gmail.com>
2020-10-28 20:48:09 +0100

    add manual upload

--- scripts/buildfire/services/publicFiles/publicFiles.js
+++ scripts/buildfire/services/publicFiles/publicFiles.js
@@ -5,7 +5,7 @@ if (typeof (buildfire.services) == "undefined") buildfire.services = {};
 if (typeof (buildfire.services.publicFiles) == "undefined")
     buildfire.services.publicFiles = {};
 
-buildfire.services.publicFiles.showDialog = function(options,onProgress ,onComplete, callback) {
+buildfire.services.publicFiles.showDialog = function (options, onProgress, onComplete, callback) {
     buildfire.eventManager.clear('publicFilesOnProgress');
     buildfire.eventManager.clear('publicFilesOnComplete');
 
@@ -46,4 +46,26 @@ buildfire.services.publicFiles._triggerOnProgress = function (data) {
 
 buildfire.services.publicFiles._triggerOnComplete = function (data) {
     buildfire.eventManager.trigger('publicFilesOnComplete', data);
+}
+
+buildfire.services.publicFiles.uploadFiles = function (files, options, onProgress, onComplete, callback) {
+    buildfire.eventManager.clear('publicFilesOnProgress');
+    buildfire.eventManager.clear('publicFilesOnComplete');
+
+    if (typeof onProgress == "function") {
+        buildfire.eventManager.add('publicFilesOnProgress', function (data) {
+            onProgress(data);
+        }, false);
+    }
+
+    if (typeof onComplete == "function") {
+        buildfire.eventManager.add('publicFilesOnComplete', function (data) {
+            onComplete(data);
+        }, false);
+    }
+
+    if (!options) options = {};
+    options.files = files;
+    var p = new Packet(null, 'publicFiles.uploadFilesManually', options);
+    buildfire._sendPacket(p, callback);
 }
