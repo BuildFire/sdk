847c696e6a09e0e545116f3e112d0a3acb7b8115
Faruk Alibasic <faruk.alibasic@gmail.com>
2020-11-09 16:12:43 +0100

    fix rating element selection issues

--- scripts/tinymce/plugins/rating/plugin.js
+++ scripts/tinymce/plugins/rating/plugin.js
@@ -1,5 +1,31 @@
 
 tinymce.PluginManager.add("rating", function (editor, url) {
+  let onNodeSelected = (e) => {
+    if (!editor.selection.isCollapsed()) return;
+    if (!e || !e.element) return
+    let ratingComponent;
+    if (e && e.element) {
+      if (e.element.dataset && e.element.className == "rating-system-tinymce") {
+        ratingComponent = e.element;
+      }
+      if (e.element.nodeName == "SPAN" && e.element.parentElement && e.element.parentElement.className == "rating-system-tinymce") {
+        ratingComponent = e.element.parentElement;
+      }
+    }
+    if (ratingComponent) {
+      if (ratingComponent.nextElementSibling) {
+        editor.selection.select(ratingComponent.nextElementSibling, true);
+        editor.selection.collapse(true);
+      } else {
+        let el = editor.dom.create("p");
+        editor.dom.insertAfter(el, ratingComponent)
+        editor.selection.select(el, true);
+        editor.selection.collapse(true);
+      }
+    }
+  }
+  editor.on("NodeChange", onNodeSelected)
+  editor.on('click', onNodeSelected);
   editor.addButton("rating", {
     text: "Rating",
     icon: false,
@@ -21,11 +47,12 @@ tinymce.PluginManager.add("rating", function (editor, url) {
               }
               let val = getElementById('data_id').value.trim();
               val = escape(val);
+
               editor.insertContent(
                 `<img src="trigger_errorasd" onerror="typeof buildfire !== 'undefined' && buildfire.ratingSystem.inject()" style="display: none">
-                  <div data-rating-id="${val}" style="text-align: center;" data-mce-style="text-align: center;">
-                  <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
-                  </div><br>`
+                                  <div class="rating-system-tinymce" data-rating-id="${val}" style="text-align: center;" data-mce-style="text-align: center;">
+                                  <span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>
+                              </div><br>`
               );
               modal.close();
             }
