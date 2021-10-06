e86da79258739904273692b138220f3892bdd83a
ahasnawi <70672687+ahasnawi@users.noreply.github.com>
2021-10-07 00:16:35 +0300

    finish tinymce layouts

--- scripts/tinymce/plugins/bf_buttons/plugin.js
+++ scripts/tinymce/plugins/bf_buttons/plugin.js
@@ -44,6 +44,7 @@ tinymce.PluginManager.add("bf_buttons", function (editor, url) {
     });
     editor.ui.registry.addContextMenu('bf_buttonOrLinkContextMenu', {
         update: function (element) {
+            editor.execCommand('mceSelectNode', false, element);
             let elementClasses = element.className;
             return elementClasses.includes('bf-btn') || elementClasses.includes('bf-text-') ? 'bf_editButtonOrLink bf_editButtonOrLinkAction' : '';
         }
