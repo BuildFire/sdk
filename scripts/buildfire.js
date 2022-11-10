8326fa4f234e1fd44b8ac25f3fcd60c6ff149374
Mahmoud AlSharif <mahmoud.k.alsharif@gmail.com>
2022-11-10 19:28:41 +0300

    fix(sdk): rating system back button check if visible

--- scripts/buildfire.js
+++ scripts/buildfire.js
@@ -1032,6 +1032,10 @@ var buildfire = {
 				var p = new Packet(null, 'appearance.titlebar.hide');
 				buildfire._sendPacket(p, callback);
 			},
+			isVisible: function(options, callback) {
+				var p = new Packet(null, 'appearance.titlebar.isVisible');
+				buildfire._sendPacket(p, callback);
+			},
 		}, navbar: {
 			show: function(options, callback) {
 				var p = new Packet(null, 'appearance.navbar.show');
