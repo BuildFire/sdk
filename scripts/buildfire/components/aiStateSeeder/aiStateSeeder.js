c707c1e784fd88825a53ee9d4f320ded1d8aaf57
Mahmoud AlSharif <mahmoud.k.alsharif@gmail.com>
2023-07-12 21:51:25 +0300

    feat(sdk): preparing import changes for ai seeder component

--- scripts/buildfire/components/aiStateSeeder/aiStateSeeder.js
+++ scripts/buildfire/components/aiStateSeeder/aiStateSeeder.js
@@ -3,44 +3,42 @@ if (typeof buildfire.components === 'undefined') buildfire.components = {};
 
 buildfire.components.aiStateSeeder = class AiStateSeeder {
 	constructor(options) {
-		if (!options.userMessage) throw new Error('userMessage parameter is required');
-		if (!options.jsonTemplate) throw new Error('jsonTemplate parameter is required');
+		// if (!options.userMessage) throw new Error('userMessage parameter is required');
+		// if (!options.jsonTemplate) throw new Error('jsonTemplate parameter is required');
 		this.options = options;
 	}
 
 	// eslint-disable-next-line no-unused-vars
 	request(options = {}, callback) {
 		const status = { isReady: false };
-		const { jsonTemplate, userMessage, systemMessage, emptyStateElement } = this.options;
 
 		if (!document.body) throw new Error('Cannot find body element');
-		if (!userMessage) throw new Error('userMessage parameter is required');
-		if (!jsonTemplate) throw new Error('jsonTemplate parameter is required');
+		if (!options.userMessage) throw new Error('userMessage parameter is required');
+		if (!options.jsonTemplate) throw new Error('jsonTemplate parameter is required');
 		if (!callback) throw new Error('callback parameter is required');
 
 		buildfire.lazyLoadScript(
 			{ relativeScriptsUrl: 'buildfire/services/ai/ai.js', scriptId: 'ai' },
 			() => {
-				const packet = new Packet(null, 'ai.showSeederPrompt', { userMessage });
+				const packet = new Packet(null, 'ai.showSeederPrompt', { userMessage: options.userMessage });
 
 				buildfire._sendPacket(packet, (err, result) => {
 					// in case user requested to regenerate, dialog should appear with the last typed message
 					if (result && result.userMessage) {
-						this.options.userMessage = result.userMessage;
+						options.userMessage = result.userMessage;
 					}
-
-					AiStateSeeder._startAIAnimation(emptyStateElement);
+					AiStateSeeder._startAIAnimation(options.emptyStateElement);
 
 					const conversation = new buildfire.ai.conversation();
-					if (systemMessage) conversation.systemSays(systemMessage);
-					if (userMessage) conversation.userSays(this.options.userMessage);
+					if (options.systemMessage) conversation.systemSays(options.systemMessage);
+					if (options.userMessage) conversation.userSays(options.userMessage);
 
-					conversation.fetchJsonResponse({ jsonTemplate }, (err, response) => {
+					conversation.fetchJsonResponse({ jsonTemplate: options.jsonTemplate }, (err, response) => {
 						status.complete = () => {
-							if (emptyStateElement) {
-								emptyStateElement.style.display = 'none';
+							if (options.emptyStateElement) {
+								options.emptyStateElement.style.display = 'none';
 							}
-							AiStateSeeder._stopAIAnimation(emptyStateElement);
+							AiStateSeeder._stopAIAnimation(options.emptyStateElement);
 							// show toast asap
 							// todo below is temporary to allow testing re-generation in the plugins
 							buildfire.dialog.toast({
@@ -51,8 +49,8 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 								actionButton: {
 									text: 'Regenerate',
 									action: () => {
-										this.options.emptyStateElement.style.display = 'block';
-										this.request(null, callback);
+										options.emptyStateElement.style.display = 'block';
+										this.request(options, callback);
 									},
 								},
 							});
@@ -61,11 +59,14 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 						callback(err, response);
 					});
 				});
-			});
+			}
+		);
 
 		return status;
 	}
 
+	// if no selector is provided then body is the default
+	// buttons visibility are configurable
 	showEmptyState({ selector } = {}, callback) {
 		const emptyStateContainer = document.querySelector(selector);
 
@@ -86,9 +87,9 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 		// in the future, we can generate multiple buttons for each seeder
 		// if the plugin developer passes a certain request or specifies certain seeder we show only one
 		emptyStateElement.querySelector('button').onclick = () => {
-			result.requestResult = this.request(null, callback);
+			result.requestResult = this.request(this.options.generateOptions, callback);
 		};
-		this.options.emptyStateElement = emptyStateElement;
+		this.options.generateOptions.emptyStateElement = emptyStateElement;
 		emptyStateContainer.appendChild(emptyStateElement);
 		return result;
 	}
@@ -96,7 +97,7 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 	static _startAIAnimation(emptyStateElement) {
 		const animationElement = AiStateSeeder._createAIAnimationElement();
 		if (emptyStateElement) {
-			emptyStateElement.insertBefore( animationElement, emptyStateElement.querySelector('.ai-empty-state-content'));
+			emptyStateElement.insertBefore(animationElement, emptyStateElement.querySelector('.ai-empty-state-content'));
 		} else {
 			animationElement.classList.add('ai-progress-overlay');
 			document.body.append(animationElement);
@@ -104,7 +105,7 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 	}
 
 	static _createAIAnimationElement() {
-		let animationElement =  document.createElement('div');
+		const animationElement =  document.createElement('div');
 		animationElement.classList.add('well');
 		animationElement.classList.add('ai-progress');
 		animationElement.innerHTML = `<div class="ai-animation">
@@ -119,10 +120,10 @@ buildfire.components.aiStateSeeder = class AiStateSeeder {
 
 	static _stopAIAnimation(emptyStateElement) {
 		if (emptyStateElement) {
-			let progressElement = emptyStateElement.querySelector('.ai-progress');
+			const progressElement = emptyStateElement.querySelector('.ai-progress');
 			progressElement.parentElement.removeChild(progressElement);
 		} else {
-			let progressElement = document.querySelector('.ai-progress-overlay');
+			const progressElement = document.querySelector('.ai-progress-overlay');
 			progressElement.parentElement.removeChild(progressElement);
 		}
 	}
