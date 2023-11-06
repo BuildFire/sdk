15c20d8be05f048f8cfe21c3fee51c750dc2b516
Bashar <bashar.kalaldeh@gmail.com>
2023-11-06 18:58:03 +0300

    Update plugin.js

--- scripts/tinymce/plugins/bf_layouts/plugin.js
+++ scripts/tinymce/plugins/bf_layouts/plugin.js
@@ -211,6 +211,7 @@ tinymce.PluginManager.add('bf_layouts', function (editor, url) {
 			let repeatedElement = layoutWrapperElement.querySelector(layoutData.repeatedSelector);
 			if (repeatedElement) {
 				repeatedElement.setAttribute('buildfire-repeat', `layoutItem in ${repeaterArray}`);
+				// remove followed non repeating elements when the original layout has multiple (grid for example) to allow repeating of one only  
 				let nonRepeatingElements = layoutWrapperElement.querySelectorAll(layoutData.repeatedSelector + ':not([buildfire-repeat])');
 				nonRepeatingElements.forEach(el => el.remove());
 			} else if (layoutData.repeatedSelector == `div#${layoutData.id}`) {
