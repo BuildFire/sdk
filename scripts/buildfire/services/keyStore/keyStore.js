71e1b065e73280355aba386a1a0ca8b04e191881
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2025-10-22 23:51:05 +0300

    feat(keyStore): add startKeyPairChange method

--- scripts/buildfire/services/keyStore/keyStore.js
+++ scripts/buildfire/services/keyStore/keyStore.js
@@ -1,4 +1,4 @@
-if (typeof (buildfire) === 'undefined') throw ('please add buildfire.js first to use BuildFire services');
+if (typeof (buildfire) === 'undefined') throw ('please add buildfire.js first to use Buildfire services');
 
 if (typeof (buildfire.services) === 'undefined') buildfire.services = {};
 if (typeof (buildfire.services.keyStore) === 'undefined') buildfire.services.keyStore = {};
@@ -8,7 +8,7 @@ if (typeof (buildfire.services.keyStore) === 'undefined') buildfire.services.key
  *  * Generate, reset, or retrieve a key pair for the currently authenticated user.
  *  * @param {Object} options Configuration options for the key pair setup.
  *  * @param {string} [options.id='default'] Optional identifier for the key pair.
- *   * @param {Object} Language Optional custom labels for UI texts.
+ *   * @param {Object} translations Optional custom labels for UI texts.
  *  * @param {Function} Callback to handle the response.
  *  */
 
@@ -23,9 +23,8 @@ buildfire.services.keyStore = {
       options.id = 'default'
     }
     
-    options.screenType = 'entry';
     const packetId = null;
-    const command = "keyStore.showPinCode";
+    const command = "keyStore.startKeyPairSetup";
     
     const packet = new Packet(packetId, command, options);
     
@@ -42,10 +41,26 @@ buildfire.services.keyStore = {
       options.id = 'default'
     }
 
-    options.screenType = 'unlock';
+    const packetId = null;
+    const command = "keyStore.startKeyPairRecovery";
+    
+    const packet = new Packet(packetId, command, options);
+    
+    buildfire._sendPacket(packet, callback);
+  },
+  
+  startKeyPairChange: function (options, callback) {
+    if (!options) {
+      callback({'code': 'error', 'message': 'Options is required'}, null);
+      return;
+    }
+    
+    if (!options.id) {
+      options.id = 'default'
+    }
     
     const packetId = null;
-    const command = "keyStore.showPinCode";
+    const command = "keyStore.startKeyPairChange";
     
     const packet = new Packet(packetId, command, options);
     
