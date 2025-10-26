3a03666b0bb852b7708c8a603de261338b61c1d4
alialaqrabawi3 <aalaqrabawi@madaincorp.com>
2025-10-26 23:45:10 +0300

    feat(keyStore): add get method to retrieve key data

--- scripts/buildfire/services/keyStore/keyStore.js
+++ scripts/buildfire/services/keyStore/keyStore.js
@@ -3,64 +3,85 @@ if (typeof (buildfire) === 'undefined') throw ('please add buildfire.js first to
 if (typeof (buildfire.services) === 'undefined') buildfire.services = {};
 if (typeof (buildfire.services.keyStore) === 'undefined') buildfire.services.keyStore = {};
 
-/**
- * /**
- *  * Generate, reset, or retrieve a key pair for the currently authenticated user.
- *  * @param {Object} options Configuration options for the key pair setup.
- *  * @param {string} [options.id='default'] Optional identifier for the key pair.
- *   * @param {Object} translations Optional custom labels for UI texts.
- *  * @param {Function} Callback to handle the response.
- *  */
-
 buildfire.services.keyStore = {
-  startKeyPairSetup: function (options, callback) {
+  
+  
+  /**
+   * /**
+   *  * @param {Object} options Configuration options for the key pair setup.
+   *   * @param {string} options.id.
+   *  * @param {Function} Callback to handle the response.
+   *  */
+  get: function (options, callback) {
     if (!options) {
       callback({'code': 'error', 'message': 'Options is required'}, null);
       return;
     }
     
-    if (!options.id) {
-      options.id = 'default'
+    const packetId = null;
+    const command = "keyStore.get";
+    
+    const packet = new Packet(packetId, command, options);
+    
+    buildfire._sendPacket(packet, callback);  },
+  
+  /**
+   * /**
+   *  * @param {Object} options Configuration options for the key pair setup.
+   *   * @param {string} options.allowCancel.
+   *   * @param {Object} translations Optional custom labels for UI texts.
+   *  * @param {Function} Callback to handle the response.
+   *  */
+  initiateSetup: function (options, callback) {
+    if (!options) {
+      callback({'code': 'error', 'message': 'Options is required'}, null);
+      return;
     }
     
     const packetId = null;
-    const command = "keyStore.startKeyPairSetup";
+    const command = "keyStore.initiateSetup";
     
     const packet = new Packet(packetId, command, options);
     
     buildfire._sendPacket(packet, callback);
   },
   
-  startKeyPairRecovery: function (options, callback) {
+  /**
+   * /**
+   *  * @param {Object} options Configuration options for the key pair setup.
+   *   * @param {string} options.allowCancel.
+   *   * @param {Object} translations Optional custom labels for UI texts.
+   *  * @param {Function} Callback to handle the response.
+   *  */
+  initiateRecovery: function (options, callback) {
     if (!options) {
       callback({'code': 'error', 'message': 'Options is required'}, null);
       return;
-    }
-    
-    if (!options.id) {
-      options.id = 'default'
     }
 
     const packetId = null;
-    const command = "keyStore.startKeyPairRecovery";
+    const command = "keyStore.initiateRecovery";
     
     const packet = new Packet(packetId, command, options);
     
     buildfire._sendPacket(packet, callback);
   },
   
-  startKeyPairChange: function (options, callback) {
+  /**
+   * /**
+   *  * @param {Object} options Configuration options for the key pair setup.
+   *   * @param {string} options.allowCancel.
+   *   * @param {Object} translations Optional custom labels for UI texts.
+   *  * @param {Function} Callback to handle the response.
+   *  */
+  initiatePinChange: function (options, callback) {
     if (!options) {
       callback({'code': 'error', 'message': 'Options is required'}, null);
       return;
     }
     
-    if (!options.id) {
-      options.id = 'default'
-    }
-    
     const packetId = null;
-    const command = "keyStore.startKeyPairChange";
+    const command = "keyStore.initiatePinChange";
     
     const packet = new Packet(packetId, command, options);
     
